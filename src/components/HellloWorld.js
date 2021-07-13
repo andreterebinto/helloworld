b16f0a1f60c868076b74a805a7fb434613be943c
Maylon Zanardi <maylon.zanardi@spro.com.br>
2021-07-13 15:57:12 -0300

    test

--- src/components/HellloWorld.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function HelloWorld() {
-  return <h1>Hello World2</h1>;
-}
-
-export default HelloWorld;
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import HelloWorld from "./components/HellloWorld";
-
-export { HelloWorld };
